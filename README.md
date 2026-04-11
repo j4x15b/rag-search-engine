cd8acfe7b2ee29169d9fd58f2ff9fa166d526963
j4x15b <jaykoblearner@proton.me>
2026-04-12 00:21:14 +0200

    refined search, tokenized search words. If one of several words is in the title, it's a hit.

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 ##########################################
 
-In this course (learn retrieval augmentation) I'm gonna build a RAG-Search Engine. 
-I'm gonna use this knowledge to build my own project, my Podcast-RAG.
+RAG Search Engine | Python, LLMs, Vector Search
+
+Built a semantic search engine powering a Netflix-like streaming service as part of Boot.dev's RAG course. The project covers the full Retrieval Augmented Generation pipeline:
+
+Ingesting and indexing a real movie dataset
+Implementing semantic search using vector embeddings
+Augmenting LLM prompts with retrieved context
+Generating accurate, context-aware responses beyond simple keyword matching
+
+###########################################
+
+In this project at first I follow along a boot.dev course: [Learn retrieval augmentation](https://www.boot.dev/courses/learn-retrieval-augmented-generation) to build a RAG-Search Engine for movies. 
+
+I'm gonna use this knowledge to build my own project, my Podcast-RAG, where you can train the AI on podcast content and 
+use it as a intelligent search.
 
 ##########################################
 
 Readme:
 
+source:
+[movie-database from boot.dev](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/course-rag-movies.json)
+
 use virtual environment:
 source .venv/bin/activate
 
-use cli to search a movie title, prints out top 5 hits from /data/movies.json:
+use cli to search a movie title, prints out top 5 hits from /data/movies.json
 
 use:
 
--- cli/search.py
+++ cli/search.py
@@ -5,33 +5,43 @@ import json
 import string
 from search_utils import DEFAULT_SEARCH_LIMIT, load_movies
 
-def tokenize(query: str) -> list[str]:
-    # input string, split, clean and put in list
-    # .split() on whitespace
-    # Remove any empty tokens
-    pass
-
-def search_command(search_word: str, list_limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
-    search_result = []
-    
+def clean_query(query: str) -> str:
     remove_punctuation = str.maketrans("", "", string.punctuation)
-    
     # make translation table, delete all punctuations:
     # string.punctuation = !"#$%&'()*+,-./:;<=>?@[]^_`{|}~\
     # make translation table, e.g. str.maketrans(from, to, delete)
     # str.maketrans("aeiou", "AEIOU", "!?.")
-
-    cleaned_search_word = search_word.translate(remove_punctuation).lower()
+    cleaned_query = query.translate(remove_punctuation).lower()
     
+    return cleaned_query
 
+def tokenize(query: str) -> list[str]:
+    # input string, split, clean and put in list
+    # .split() on whitespace
+    # Remove any empty tokens
+    tokenized_query = []
+    temp_list = query.split()
+    for item in temp_list:
+        if item:
+            tokenized_query.append(item)
+
+    return tokenized_query
+
+def search_command(search_word: str, list_limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
+    search_result = []
+    
+    cleaned_query = clean_query(search_word)
+    tokenized_query = tokenize(cleaned_query)
 
     movie_data = load_movies()
     # search movie titles: cleaned search word and cleaned movie title
     for movie in movie_data:
-        cleaned_title = movie["title"].translate(remove_punctuation).lower()
-        if cleaned_search_word in cleaned_title:
-            search_result.append(movie)
-            if len(search_result) >= list_limit:
+        cleaned_title = clean_query(movie["title"])
+        for token in tokenized_query:
+            if token in cleaned_title:
+                search_result.append(movie)
                 break
+        if len(search_result) >= list_limit:
+            break
 
     return search_result
